ac66cbc225559312aa956530668d12c3c3a590bc
ShmuelAppleton <shmuelappleton@gmail.com>
2022-03-25 11:32:28 +0300

    initial basic update from example plugin

--- src/test/java/com/releaserestricted/ReleaseRestrictedPluginTest.java
+++ src/test/java/com/releaserestricted/ReleaseRestrictedPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.releaserestricted;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ReleaseRestrictedPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ReleaseRestrictedPlugin.class);
 		RuneLite.main(args);
 	}
 }
